687589216f19a3e73eded081eae91997014bec11
lena-tkachenko <lena.tkachenko.physic@gmail.com>
2020-11-29 15:22:52 +0200

    small changes

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
       <div className="logo-title">
-          <h1>Mind<span className="change-color">y.</span></h1>
+          <h1>Mind<span className="change-color">y</span>.</h1>
       </div>
   );
 }
